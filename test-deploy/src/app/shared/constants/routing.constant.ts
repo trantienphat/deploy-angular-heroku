d458635a199f202736496adbc4af98b14d9ab79a
Tran Tien Phat <trantienphat1110@gmail.com>
2018-08-02 04:49:41 +0700

    Somthing

--- test-deploy/src/app/shared/constants/routing.constant.ts
+++ test-deploy/src/app/shared/constants/routing.constant.ts
@@ -1,7 +1,7 @@
 export class PageName {
     public static LOGIN_PAGE = 'login';
     public static DASHBOARD_PAGE = 'dashboard';
-    public static ACCOUNT_PAGE = 'dashboard';
+    public static ACCOUNT_PAGE = 'accout';
     public static CHANGE_PASSWORD_PAGE = 'changepassword';
     public static LISTING_TUTOR_PAGE = 'listingtutor';
     public static LISTING_STUDENT_PAGE = 'listingstudent';
