22731e0cf1ea8c32484981a1c43bf7fd910fbbe0
ORIENTSOFTWARE\ttphat <phat.tran@orientsoftware.net>
2018-08-01 15:52:05 +0700

    Init ngb & Dashboar-page

--- test-deploy/src/app/app.module.ts
+++ test-deploy/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -23,7 +25,9 @@ import { DashboardPageComponent } from './containers/dashboard-page/dashboard-pa
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule.forRoot(),
+    ReactiveFormsModule,
   ],
   providers: [SharedService],
   bootstrap: [AppComponent]
--- test-deploy/src/app/containers/dashboard-page/dashboard-page.component.ts
+++ test-deploy/src/app/containers/dashboard-page/dashboard-page.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPageComponent implements OnInit {
 
+  public userName = 'Username';
+
   constructor() { }
 
   ngOnInit() {
