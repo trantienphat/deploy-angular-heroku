1887f2a0704b5d80d07ffc8b66a94a1f79b193ca
Tran Tien Phat <trantienphat1110@gmail.com>
2018-08-02 05:39:07 +0700

    Change password

--- test-deploy/src/app/containers/change-password-page/change-password-page.component.ts
+++ test-deploy/src/app/containers/change-password-page/change-password-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedService } from '../../shared/services/shared.service';
+import { PageName } from '../../shared/constants/routing.constant';
 
 @Component({
   selector: 'app-change-password-page',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangePasswordPageComponent implements OnInit {
 
-  constructor() { }
+  public userName = 'Username';
+
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
   }
 
+  onClickAccountButton() {
+    this.sharedService.routingToPage(PageName.ACCOUNT_PAGE);
+  }
+
+  onClickChangePasswordButton() {
+    this.sharedService.routingToPage(PageName.CHANGE_PASSWORD_PAGE);
+  }
+
+  onClickDashboardButton() {
+    this.sharedService.routingToPage(PageName.DASHBOARD_PAGE);
+  }
+
+  onClickListingTutorsButton() {
+    this.sharedService.routingToPage(PageName.LISTING_TUTOR_PAGE);
+  }
+
+  onClickListingStudentsButton() {
+    this.sharedService.routingToPage(PageName.LISTING_STUDENT_PAGE);
+  }
+
+  onClickListingRequisitionsButton() {
+    this.sharedService.routingToPage(PageName.LISTING_REQUISITION_PAGE);
+  }
+
+  onClickLogoutButton() {
+    // Code here
+  }
 }
