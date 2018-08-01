596d84465bd2a9fa9c64d003c2d30f4da852c6c0
Tran Tien Phat <trantienphat1110@gmail.com>
2018-08-02 05:54:33 +0700

    Init all page

--- test-deploy/src/app/containers/listing-requisitions-page/listing-requisitions-page.component.ts
+++ test-deploy/src/app/containers/listing-requisitions-page/listing-requisitions-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PageName } from '../../shared/constants/routing.constant';
+import { SharedService } from '../../shared/services/shared.service';
 
 @Component({
   selector: 'app-listing-requisitions-page',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListingRequisitionsPageComponent implements OnInit {
 
-  constructor() { }
+  // public array = [];
+
+  // public count = 0;
+
+  // public string = '{"response": [{"A":"A"},{"A":"A"},{"A":"A"}]}';
+
+  public userName = 'Username';
+
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
+    // const x = JSON.parse(this.string);
+    // this.array = x.response;
+    // this.count = this.array.length;
+    // console.log(this.count);
+  }
+
+  onClickAccountButton() {
+    this.sharedService.routingToPage(PageName.ACCOUNT_PAGE);
+  }
+
+  onClickChangePasswordButton() {
+    this.sharedService.routingToPage(PageName.CHANGE_PASSWORD_PAGE);
+  }
+
+  onClickDashboardButton() {
+    this.sharedService.routingToPage(PageName.DASHBOARD_PAGE);
+  }
+
+  onClickListingTutorsButton() {
+    this.sharedService.routingToPage(PageName.LISTING_TUTOR_PAGE);
+  }
+
+  onClickListingStudentsButton() {
+    this.sharedService.routingToPage(PageName.LISTING_STUDENT_PAGE);
+  }
+
+  onClickListingRequisitionsButton() {
+    this.sharedService.routingToPage(PageName.LISTING_REQUISITION_PAGE);
+  }
+
+  onClickLogoutButton() {
+    // Code here
   }
 
 }
