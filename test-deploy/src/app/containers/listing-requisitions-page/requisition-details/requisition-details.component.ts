e94e9157e025ed731ff2e0ebce7d95e78dcdf881
ORIENTSOFTWARE\ttphat <phat.tran@orientsoftware.net>
2018-08-06 09:26:56 +0700

    Init details requisition

--- test-deploy/src/app/containers/listing-requisitions-page/requisition-details/requisition-details.component.ts
+++ test-deploy/src/app/containers/listing-requisitions-page/requisition-details/requisition-details.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { SharedService } from '../../../shared/services/shared.service';
+import { AuthService } from '../../../auth/auth.service';
+import { RequisitionCourseService } from '../../../shared/services/requisition-course.service';
+import { User } from '../../../shared/models/user.model';
+import { CommonConstants } from '../../../shared/constants/common.constant';
 
 @Component({
   selector: 'app-requisition-details',
@@ -9,11 +14,40 @@ import { ActivatedRoute } from '@angular/router';
 export class RequisitionDetailsComponent implements OnInit {
 
   public id = '';
+  public user = new User();
 
-  constructor(private activatedRoute: ActivatedRoute) {
-    this.activatedRoute.queryParams.subscribe(param => {
-      this.id = param.id;
-    });
+  constructor(private activatedRoute: ActivatedRoute,
+    private sharedService: SharedService,
+    private authService: AuthService,
+    private requisitionService: RequisitionCourseService) {
+
+    this.checkAccessPage();
+
+  }
+
+  checkAccessPage() {
+    if ( !this.authService.checkAuthentication()) {
+      this.authService.logout();
+    } else {
+      this.initPage();
+    }
+  }
+
+  initPage() {
+    const _user = JSON.parse(window.localStorage.getItem(CommonConstants.userInfo));
+    if ( !_user) {
+      this.authService.logout();
+    } else {
+      this.user = _user;
+      this.activatedRoute.queryParams.subscribe(param => {
+        this.id = param.id;
+      });
+      this.getRequisitionsCourseById();
+    }
+  }
+
+  getRequisitionsCourseById() {
+
   }
 
   ngOnInit() {
